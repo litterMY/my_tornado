8ef927e54b75ae123d2ea22b78e931b80dad9203
minyang <minyang1991@gmail.com>
2018-07-26 16:58:33 +0800

    自动重启的功能，貌似不推荐使用。有些资源不能随便释放。热更新的话还是通过负载均衡来做比较稳妥。

---
+++ autoreload.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+if __name__ == "__main__":
+    if sys.path[0]==os.path.dirname(__file__):
+        del sys.path[0]
+
+import functools
+import logging
+import os
+import pkgutil
+import sys
+import traceback
+import types
+import subprocess
+import weakref
+
+from tornado import ioloop
+from tornado.log import gen_log
+from tornado import process
+from tornado.util import exec_in
+
+try:
+    import signal
+
+except ImportError:
+    signal = None
+
+
+_has_execv = sys.platform != 'win32'
+_reload_hooks = []
+_reload_attempted = False
+_io_loops = weakref.WeakKeyDictionary()
+_autoreload_is_main = False
+_original_argv = None
+_original_spec = None
+
+def start(check_time=500):
+    """
+    开始观察源文件的变动
+    """
+    io_loop = ioloop.IOLoop.current()
+    if io_loop in _io_loops:
+        return
+    _io_loops[io_loop] = True
+    if(len(_io_loops))>1:
+        gen_log.warning("tornado.autoreload started more than once in the same process")
+    modify_times={}
+    callback = functools.partial(_reload_on_update, modify_times)
+    scheduler = ioloop.PeriodicCallback(callback, check_time)
+    scheduler.start()
